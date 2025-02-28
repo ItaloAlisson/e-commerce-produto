a2672338379f0a6790cfa04efe8355930f2e4689
Italo Alisson <italoalisson15@gmail.com>
2025-02-27 22:08:35 -0300

    :sparkles: feat: adiciona ProdutoModelElasticSearch e configuração

---
+++ src/main/java/com/ecommerce/produto/models/ProdutoModelElasticSearch.java
@@ -0,0 +1,39 @@
+package com.ecommerce.produto.models;
+
+import com.ecommerce.produto.enums.CategoriaEnum;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(indexName = "produtos")
+public class ProdutoModelElasticSearch {
+
+    @Id
+    @Field(type = FieldType.Keyword)
+    private UUID id;
+    @Field(type = FieldType.Text)
+    private String nome;
+    @Field(type = FieldType.Text)
+    private String marca;
+    @Field(type = FieldType.Double)
+    private Double preco;
+    @Field(type = FieldType.Integer)
+    private Integer quantidade;
+    @Field(type = FieldType.Keyword)
+    private CategoriaEnum categoria;
+    @Field(type = FieldType.Text)
+    private String descricao;
+
+
+}
