bd15f1f31b9001f689d01599e477153cc3e8e0c1
Italo Alisson <italoalisson15@gmail.com>
2025-02-27 21:33:18 -0300

    :sparkles: feat: adiciona ProdutoModel e configurações do banco

---
+++ src/main/java/com/ecommerce/produto/enums/CategoriaEnum.java
@@ -0,0 +1,17 @@
+package com.ecommerce.produto.enums;
+
+public enum CategoriaEnum {
+    ELETRONICOS,
+    MOVEIS,
+    TECNOLOGIA,
+    ROUPAS,
+    ALIMENTOS,
+    BELEZA,
+    CASA,
+    ESPORTES,
+    LIVROS,
+    BRINQUEDOS,
+    PETS
+
+
+}
