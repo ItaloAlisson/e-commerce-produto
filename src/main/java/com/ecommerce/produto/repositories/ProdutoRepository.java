a8e912f00180e31944275304d2dfec5891f05adf
Italo Alisson <italoalisson15@gmail.com>
2025-02-28 20:59:50 -0300

    :sparkles: feat: adiciona ProdutoValidator

--- src/main/java/com/ecommerce/produto/repositories/ProdutoRepository.java
+++ src/main/java/com/ecommerce/produto/repositories/ProdutoRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, UUID> {
+    boolean existsByNome(String nome);
 }
