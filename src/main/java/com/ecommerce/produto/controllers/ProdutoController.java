89e096b8313ef4e4e4479139cb68ef091cf57124
Italo Alisson <italoalisson15@gmail.com>
2025-02-27 22:21:44 -0300

    :sparkles: feat: adiciona ProdutoController

---
+++ src/main/java/com/ecommerce/produto/controllers/ProdutoController.java
@@ -0,0 +1,16 @@
+package com.ecommerce.produto.controllers;
+
+import com.ecommerce.produto.services.ProdutoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+}
