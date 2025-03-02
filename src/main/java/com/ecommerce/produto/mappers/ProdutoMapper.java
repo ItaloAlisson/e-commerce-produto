33d3744290f7d79fbe3d44a622f52ebe42914c36
Italo Alisson <italoalisson15@gmail.com>
2025-03-02 17:33:23 -0300

    :sparkles: feat: adiciona mapper de produto model para model elasticsearch

--- src/main/java/com/ecommerce/produto/mappers/ProdutoMapper.java
+++ src/main/java/com/ecommerce/produto/mappers/ProdutoMapper.java
@@ -2,10 +2,13 @@ package com.ecommerce.produto.mappers;
 
 import com.ecommerce.produto.dtos.ProdutoRecordDTO;
 import com.ecommerce.produto.models.ProdutoModel;
+import com.ecommerce.produto.models.ProdutoModelElasticSearch;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ProdutoMapper {
 
-    ProdutoModel produtoDTOParaModel(ProdutoRecordDTO produtoDTO);
+    ProdutoModel produtoDTOParaProdutoModel(ProdutoRecordDTO produtoDTO);
+
+    ProdutoModelElasticSearch produtoModelParaModelElasticSearch(ProdutoModel produtoModel);
 }
