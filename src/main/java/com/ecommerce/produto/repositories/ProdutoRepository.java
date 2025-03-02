50fda340c0373cbc7693fad598862fec9b6d7d6c
Italo Alisson <italoalisson15@gmail.com>
2025-03-02 17:35:45 -0300

    :bug: fix: adiciona spring annotation @Repository

--- src/main/java/com/ecommerce/produto/repositories/ProdutoRepository.java
+++ src/main/java/com/ecommerce/produto/repositories/ProdutoRepository.java
@@ -2,9 +2,10 @@ package com.ecommerce.produto.repositories;
 
 import com.ecommerce.produto.models.ProdutoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, UUID> {
     boolean existsByNome(String nome);
 }
