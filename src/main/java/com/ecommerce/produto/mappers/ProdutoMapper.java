df6b49b724399ea63f77b0288c8f2c0caa2197ff
Italo Alisson <italoalisson15@gmail.com>
2025-03-03 17:08:24 -0300

    :bug: fix: resolve problema de conversão de LocalDateTime para Instant após inclusão de redis

--- src/main/java/com/ecommerce/produto/mappers/ProdutoMapper.java
+++ src/main/java/com/ecommerce/produto/mappers/ProdutoMapper.java
@@ -4,11 +4,12 @@ import com.ecommerce.produto.dtos.ProdutoRecordDTO;
 import com.ecommerce.produto.models.ProdutoModel;
 import com.ecommerce.produto.models.ProdutoModelElasticSearch;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProdutoMapper {
 
     ProdutoModel produtoDTOParaProdutoModel(ProdutoRecordDTO produtoDTO);
-
+    @Mapping(target = "dataRegistro", ignore = true)
     ProdutoModelElasticSearch produtoModelParaModelElasticSearch(ProdutoModel produtoModel);
 }
