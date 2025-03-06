a83ed9b00793631e44be479f8c8fcf0a1b1787d9
Italo Alisson <italoalisson15@gmail.com>
2025-03-06 15:11:48 -0300

    :sparkles: feat: Adiciona TesteDataFactory

---
+++ src/test/java/com/ecommerce/produto/TestDataFactory.java
@@ -0,0 +1,44 @@
+package com.ecommerce.produto;
+
+import com.ecommerce.produto.dtos.ProdutoRecordDTO;
+import com.ecommerce.produto.enums.CategoriaEnum;
+import com.ecommerce.produto.models.ProdutoModel;
+import com.ecommerce.produto.models.ProdutoModelElasticSearch;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class TestDataFactory {
+
+    public static ProdutoRecordDTO produtoDTO(){
+        return new ProdutoRecordDTO("Moto G75","Motorola", BigDecimal.valueOf(1600), 420,
+                CategoriaEnum.TECNOLOGIA,"Primeiro moto g com ultrarrêsistencia," +
+                " O poder da IA e a câmera Sony - LYTIA 600, 5 anos de atualização de Android, Som Dolby Atmos e tela "+
+                "Full HD+ de 6,8” com superbrilho e Smart Water Touch . Velocidade e eficiência do novo Snapdragon.");
+    }
+
+    public static ProdutoModel produtoParaPersistencia(){
+        return new ProdutoModel("Moto G75","Motorola",BigDecimal.valueOf(1600),420,
+                CategoriaEnum.TECNOLOGIA,"Primeiro moto g com ultrarrêsistencia," +
+                " O poder da IA e a câmera Sony - LYTIA 600, 5 anos de atualização de Android, Som Dolby Atmos e tela "+
+                "Full HD+ de 6,8” com superbrilho e Smart Water Touch . Velocidade e eficiência do novo Snapdragon.");
+
+    }
+
+    public static ProdutoModel produtoDB(){
+        return new ProdutoModel(UUID.fromString("2c1bd9c6-ecd4-44e4-9f3d-fe54c7a56602"),"Moto G75",
+                "Motorola",BigDecimal.valueOf(1600),420,CategoriaEnum.TECNOLOGIA,
+                "Primeiro moto g com ultrarrêsistencia," +
+                        " O poder da IA e a câmera Sony - LYTIA 600, 5 anos de atualização de Android, Som Dolby Atmos e tela "+
+                        "Full HD+ de 6,8” com superbrilho e Smart Water Touch . Velocidade e eficiência do novo Snapdragon.");
+    }
+
+    public static ProdutoModelElasticSearch produtoElasticDB(){
+        return new ProdutoModelElasticSearch(UUID.fromString("2c1bd9c6-ecd4-44e4-9f3d-fe54c7a56602"),"Moto G75",
+                "Motorola",1600.00,420,CategoriaEnum.TECNOLOGIA,
+                "Primeiro moto g com ultrarrêsistencia," +
+                " O poder da IA e a câmera Sony - LYTIA 600, 5 anos de atualização de Android, Som Dolby Atmos e tela "+
+                "Full HD+ de 6,8” com superbrilho e Smart Water Touch . Velocidade e eficiência do novo Snapdragon.");
+    }
+
+}
