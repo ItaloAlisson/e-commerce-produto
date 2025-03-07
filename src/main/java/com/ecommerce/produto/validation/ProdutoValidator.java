194c218da9c0c85db55eaf1be7eb2d7e2a6521de
Italo Alisson <italoalisson15@gmail.com>
2025-03-07 00:01:22 -0300

    :white_check_mark: test: adiciona teste para método existePorNome inexistente na camada de validação

--- src/main/java/com/ecommerce/produto/validation/ProdutoValidator.java
+++ src/main/java/com/ecommerce/produto/validation/ProdutoValidator.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProdutoValidator {
 
-    private final ProdutoRepository produtoRepository;
+    private ProdutoRepository produtoRepository;
 
     public ProdutoValidator(ProdutoRepository produtoRepository) {
         this.produtoRepository = produtoRepository;
---
+++ src/test/java/com/ecommerce/produto/validation/ProdutoValidatorTest.java
@@ -0,0 +1,47 @@
+package com.ecommerce.produto.validation;
+
+import com.ecommerce.produto.dtos.ProdutoRecordDTO;
+import com.ecommerce.produto.repositories.ProdutoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static com.ecommerce.produto.TestDataFactory.produtoDTO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProdutoValidatorTest {
+
+    @InjectMocks
+    ProdutoValidator produtoValidator;
+
+    @Mock
+    ProdutoRepository produtoRepository;
+
+    private ProdutoRecordDTO produtoDTO;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        produtoDTO = produtoDTO();
+    }
+
+    @DisplayName(" Deve validar o produto com nome inexistente")
+    @Test
+    void deveValidarProdutoNomeInexistente() {
+
+        when(produtoRepository.existsByNome("Iphone 16")).thenReturn(false);
+
+        produtoValidator.existePorNome("Iphone 16");
+
+        verify(produtoRepository).existsByNome("Iphone 16");
+    }
+}
