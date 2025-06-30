9c7fbc4496f8a7571401e43a5a8d0bd383f16b78
Italo Alisson <italo.silva@basehub.com.br>
2025-06-29 21:33:28 -0300

    :sparkles: feat: adiciona mapper categoriaDTOParaCategoria

--- src/main/java/com/ecommerce/produto/mappers/ProdutoMapper.java
+++ src/main/java/com/ecommerce/produto/mappers/ProdutoMapper.java
@@ -1,6 +1,8 @@
 package com.ecommerce.produto.mappers;
 
+import com.ecommerce.produto.dtos.CategoriaRecordDTO;
 import com.ecommerce.produto.dtos.ProdutoRecordDTO;
+import com.ecommerce.produto.models.Categoria;
 import com.ecommerce.produto.models.ProdutoModel;
 import com.ecommerce.produto.models.ProdutoModelElasticSearch;
 import org.mapstruct.Mapper;
@@ -10,4 +12,6 @@ import org.mapstruct.Mapping;
 public interface ProdutoMapper {
     ProdutoModel produtoDTOParaProdutoModel(ProdutoRecordDTO produtoDTO);
     ProdutoModelElasticSearch produtoModelParaModelElasticSearch(ProdutoModel produtoModel);
+
+    Categoria categoriaDTOParaCategoria(CategoriaRecordDTO categoriaDTO);
 }
